c5f86371995e0c889f8f26e163d2e05562baf8cc
sergiMagret <u1953662@campus.udg.edu>
2020-03-25 12:44:29 +0100

    Readjusted the profile fragment to fit all screen sizes, now it's relative to the screen.

--- app/src/main/java/org/udg/pds/todoandroid/entity/User.java
+++ app/src/main/java/org/udg/pds/todoandroid/entity/User.java
@@ -18,10 +18,6 @@ public class User {
     public String description;
     public String profilePicture;
     public Integer numberPublications;
-    // No sé si s'ha de fer servir aquesta classe.
     public List<Integer> followers;
     public List<Integer> following;
-    // public List<Publication> publications; ??
-    // S'ha de crear la classe publication
-
 }
