2e1920bb80c1975d2fcced39f29ef32b4818fd53
OliuLL <61210024+OliuLL@users.noreply.github.com>
2020-06-05 11:31:09 +0200

    He arreglat un parell de coses que es podien millorar i he afegit una nova icona per l'imatge de veure els tagged users.

--- app/src/main/java/org/udg/pds/todoandroid/activity/TagPeople.java
+++ app/src/main/java/org/udg/pds/todoandroid/activity/TagPeople.java
@@ -1,17 +1,37 @@
 package org.udg.pds.todoandroid.activity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
+import android.widget.SearchView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.squareup.picasso.Picasso;
+
 import org.udg.pds.todoandroid.R;
 import org.udg.pds.todoandroid.TodoApp;
 import org.udg.pds.todoandroid.entity.Publication;
+import org.udg.pds.todoandroid.entity.User;
+import org.udg.pds.todoandroid.fragment.SearchFragment;
+import org.udg.pds.todoandroid.fragment.SearchFragmentDirections;
 import org.udg.pds.todoandroid.rest.TodoApi;
+import org.udg.pds.todoandroid.util.Global;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -21,6 +41,11 @@ import retrofit2.Response;
 public class TagPeople extends AppCompatActivity {
     TodoApi mTodoService;
     Long publicationId;
+    private TRAdapter mAdapter;
+    RecyclerView mRecyclerView;
+    Integer elemDemanats;
+    Integer elemPerPagina=20;
+    String textDemanat;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,7 +53,7 @@ public class TagPeople extends AppCompatActivity {
         mTodoService = ((TodoApp) this.getApplication()).getAPI();
         Bundle b = getIntent().getExtras();
         publicationId = b.getLong("id");
-        ImageView tagImage = findViewById(R.id.tagImage);
+        /*ImageView tagImage = findViewById(R.id.tagImage);
         tagImage.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
@@ -37,7 +62,7 @@ public class TagPeople extends AppCompatActivity {
                 username.setText("");
                 TagPeople.this.tagUser(name,publicationId);
             }
-        });
+        });*/
         ImageView finish = findViewById(R.id.checkFinish);
         finish.setOnClickListener(new View.OnClickListener(){
             @Override
@@ -48,7 +73,7 @@ public class TagPeople extends AppCompatActivity {
         });
     }
 
-    void tagUser (String username, Long publicationId){
+    void tagUser (String username){
         if(username.length()>0){
             Call<Integer> call = mTodoService.tagUser(publicationId, username);
             call.enqueue(new Callback<Integer>() {
@@ -83,4 +108,218 @@ public class TagPeople extends AppCompatActivity {
             toast.show();
         }
     }
+
+    @Override
+    public void onStart() {
+
+        super.onStart();
+        mTodoService = ((TodoApp) this.getApplication()).getAPI();
+        mRecyclerView = this.findViewById(R.id.RecyclerView_TagUser);
+
+        mAdapter = new TRAdapter(this.getApplication());
+        mRecyclerView.setAdapter(mAdapter);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        SearchView searchView = TagPeople.this.findViewById(R.id.searchView_TagUser);
+
+        searchView.setMaxWidth(Integer.MAX_VALUE);
+
+        // listening to search query text change
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                // filter recycler view when query submitted
+                updateUserList(query);
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String query) {
+                // filter recycler view when text is changed
+
+                updateUserList(query);
+                return false;
+            }
+        });
+
+        mRecyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
+                super.onScrollStateChanged(recyclerView, newState);
+
+                if (!recyclerView.canScrollVertically(1) && newState==RecyclerView.SCROLL_STATE_IDLE && mAdapter.getItemCount()==elemDemanats ) {
+
+                    Call<List<User>> call = mTodoService.getUsers(textDemanat,(elemDemanats/elemPerPagina),elemPerPagina);
+                    elemDemanats=elemDemanats+elemPerPagina;
+
+                    call.enqueue(new Callback<List<User>>() {
+                        @Override
+                        public void onResponse(Call<List<User>> call, Response<List<User>> response) {
+                            if (response.isSuccessful()) {
+                                TagPeople.this.addUserList(response.body());
+                            } else {
+                                Toast.makeText(TagPeople.this, "Error reading users", Toast.LENGTH_LONG).show();
+                            }
+                        }
+
+                        @Override
+                        public void onFailure(Call<List<User>> call, Throwable t) {
+
+                        }
+                    });
+
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        this.updateUserList("");
+    }
+
+    public void showUserList(List<User> tl) {
+        mAdapter.clear();
+        for (User t : tl) {
+            mAdapter.add(t);
+        }
+    }
+
+    public void addUserList(List<User> tl) {
+        for (User t : tl) {
+            mAdapter.add(t);
+        }
+    }
+
+    /*@Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == Global.RQ_ADD_TASK) {
+            this.updateUserList("");
+        }
+    }*/
+
+    public void updateUserList(String text) {
+        textDemanat=text;
+        elemDemanats=elemPerPagina;
+        Call<List<User>> call = mTodoService.getUsers(text,0,elemPerPagina);
+
+
+        call.enqueue(new Callback<List<User>>() {
+            @Override
+            public void onResponse(Call<List<User>> call, Response<List<User>> response) {
+                if (response.isSuccessful()) {
+                    TagPeople.this.showUserList(response.body());
+                } else {
+                    Toast.makeText(TagPeople.this, "Error reading users", Toast.LENGTH_LONG).show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<User>> call, Throwable t) {
+
+            }
+        });
+    }
+
+    static class UserViewHolder extends RecyclerView.ViewHolder {
+        TextView username;
+        TextView name;
+        ImageView  profilePicture;
+        View view;
+
+        UserViewHolder(View itemView) {
+            super(itemView);
+            view = itemView;
+            username = itemView.findViewById(R.id.itemUsername);
+            name = itemView.findViewById(R.id.itemName);
+            profilePicture = itemView.findViewById(R.id.item_profile_picture);
+        }
+    }
+
+    class TRAdapter extends RecyclerView.Adapter<UserViewHolder> {
+
+        List<User> listFiltered = new ArrayList<>();
+        Context context;
+
+
+        public TRAdapter(Context context) {
+            this.context = context;
+        }
+
+        @Override
+        public UserViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.search_layout, parent, false);
+            UserViewHolder holder = new UserViewHolder(v);
+
+            return holder;
+        }
+
+        @Override
+        public void onBindViewHolder(UserViewHolder holder, final int position) {
+
+            holder.name.setText(listFiltered.get(position).name); //list.get(position).username
+            holder.username.setText("@" + listFiltered.get(position).username); //list.get(position).username
+            Picasso.get().load(listFiltered.get(position).profilePicture).into(holder.profilePicture);
+
+
+            holder.view.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    tagUser(listFiltered.get(position).username);
+                }
+            });
+
+
+            animate(holder);
+        }
+
+        @Override
+
+        public int getItemCount() {
+            return listFiltered.size();
+        }
+
+        @Override
+        public void onAttachedToRecyclerView(RecyclerView recyclerView) {
+
+            super.onAttachedToRecyclerView(recyclerView);
+        }
+
+        // Insert a new item to the RecyclerView
+        public void insert(int position, User data) {
+            //list.add(position, data);
+            listFiltered.add(position, data);
+            notifyItemInserted(position);
+        }
+
+        // Remove a RecyclerView item containing the Data object
+        public void remove(User data) {
+            //int position = list.indexOf(data);
+            //list.remove(position);
+            int position = listFiltered.indexOf(data);
+            listFiltered.remove(position);
+            notifyItemRemoved(position);
+        }
+
+
+        public void animate(RecyclerView.ViewHolder viewHolder) {
+            final Animation animAnticipateOvershoot = AnimationUtils.loadAnimation(context, android.R.anim.slide_in_left);
+            viewHolder.itemView.setAnimation(animAnticipateOvershoot);
+        }
+
+        public void add(User t) {
+            //list.add(t);
+            listFiltered.add(t);
+            this.notifyItemInserted(listFiltered.size() - 1);
+        }
+
+        public void clear() {
+            int size = listFiltered.size();
+            //list.clear();
+            listFiltered.clear();
+            this.notifyItemRangeRemoved(0, size);
+        }
+    }
+
 }
